8a54ec44cef6c4319faad7698eee40c271d51737
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-01-29 15:38:27 -0500

    c

--- app/src/Profile.tsx
+++ app/src/Profile.tsx
@@ -1,31 +1,71 @@
 import { useState } from "react"
 
+type Class = "Warrior" | "Scholar";
+
 function Profile() {
-  // const [playerClass, setPlayerClass] = useState();
+  const [playerClass, setPlayerClass] = useState<Class>("Scholar");
   return (
     <>
       <h1>Archetype Selection</h1>
-      <ClassHeading>The Scholar</ClassHeading>
-      
-
+			{/*<ClassHeading>The {playerClass}</ClassHeading>
+			<ClassImage class_={playerClass} />
+			<ClassList selectClass=setPlayerClass />*/}
     </>
   );
 }
 
-function ClassHeading({ children: className }: { children: string }) {
+function ClassHeading({ children: class_ }: { children: string }) {
   return (
     <>
       <h1>
-        {className}
+        {class_}
       </h1>
     </>
-  )
+  );
 }
 
-function ClassImage(className: string) {
+function ClassImage(class_: Class) {
   return (
     <>
       <div></div>
     </>
-  )
+  );
 }
+
+function ClassList(selectClass: (Class) => void) {
+	const classes = ["Warrior", "Scholar"];
+	return (
+    <>
+      <ul>
+				{classes.map(class_ => (
+					<li>
+					  <SelectClassButton
+							class_={class_}
+							selectClass={selectClass}
+						/>
+					</li>
+				)}
+			</ul>
+    </>
+  );
+}
+
+function SelectClassButton(
+	class_: Class,
+	selectClass: (Class) => void
+) {
+	function onClick() {
+		selectClass(class_);
+	}
+  return (
+    <>
+			<button
+			  onClick=onClick
+			>
+			  {class_}
+			</button>
+    </>
+  );
+}
+
+export default Profile;
