f3e09f7fba9cacda267d91b74e3bb5643a5c8bb0
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-02-02 21:23:34 -0500

    c

--- api/src/index.ts
+++ api/src/index.ts
@@ -10,7 +10,7 @@ const port = 3000
 
 const users = ["John Smith", "Rebecca Wafer", "Phil Hartman"];
 
-app.use(express.static(path.join(__dirname, '../public')))
+// app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (_req, res) => {
   res.send('Hello Express!')
@@ -24,9 +24,13 @@ app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
   res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
 })
 
-// app.get('*splat', (_req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/index.html'))
-// })
+// Send all unhandled paths to the static files; they will be handled with
+// client-side routing.
+// Only needed in debug build when serving static files from express instead of
+// running the frontend dev server.
+app.get('*splat', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
