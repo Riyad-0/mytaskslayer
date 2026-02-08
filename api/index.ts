df3b63f28227a804beb3c352a41007cc7d213e4e
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-02-07 23:32:53 -0500

    c

--- api/index.ts
+++ api/index.ts
@@ -9,18 +9,24 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+app.use(express.json());
+
 const users = ["John Smith", "Rebecca Wafer", "Phil Hartman"];
 
-app.get('/', (_req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello Express!');
 });
 
-app.get('/api/users/:id', (_req, res) => {
-  res.json({ name: users[parseInt(_req.params.id)] });
+app.post('/api/login', (req, res) => {
+  res.json({ message: `Logged in`, body: req.body });
 });
 
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId });
+app.get('/api/users/:id', (req, res) => {
+  res.json({ name: users[parseInt(req.params.id)] });
+});
+
+app.get('/api/posts/:postId/comments/:commentId', (req, res) => {
+  res.json({ postId: req.params.postId, commentId: req.params.commentId });
 });
 
 app.listen(port, () => console.log(`api: http://localhost:${port}`));
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,10 +1,41 @@
+import { useState, type SetStateAction } from "react";
+
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  function onUsernameChange(e: { target: { value: SetStateAction<string>; }; }) {
+    setUsername(e.target.value);
+  }
+
+  function onPasswordChange(e: { target: { value: SetStateAction<string>; }; }) {
+    setPassword(e.target.value);
+  }
+
+  const onSubmit: React.SubmitEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    e.target.submit
+    console.log("test");
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    });
+    const body = await res.json();
+    console.log("response: ", body);
+  }
+
   return (
     <>
-      <form>
-        <input />
-        <input />
-        <button type="submit">Log in</button>
+      <form onSubmit={onSubmit}>
+        <input name="username" onChange={onUsernameChange} value={username} />
+        <input name="password" onChange={onPasswordChange} value={password} />
+        <button action type="submit">Log in</button>
       </form>
     </>
   );
