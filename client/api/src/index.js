bde891c49572a48ecbc8437dc9bf31d5e1a66729
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-05-11 18:21:26 -0400

    c

--- client/api/src/index.js
+++ client/api/src/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import app from "server";
 
 // const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/api/hello', (req, res) => {
   res.send("Hello!");
 });
 
-// app.listen(PORT, () => {
-//   console.log(`Server running: http://localhost:${PORT}/`);
-// });
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT}/`);
+});
 
 export default app;
