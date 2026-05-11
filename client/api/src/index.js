bc6d5218621738a1cf021f4c1cb01118faf38fa5
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-05-11 17:59:26 -0400

    c

---
+++ client/api/src/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import app from "server";
+
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.get('/api/hello', (req, res) => {
+//   res.send("Hello!" + " " + a);
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running: http://localhost:${PORT}/`);
+// });
+
+export default app;
--- client/vercel/src/a.js
+++
@@ -1,2 +0,0 @@
-const a = 50;
-export default a;
