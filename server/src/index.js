f14611984d13ece5b770b85338cbbf1a11bafe46
Riyad-0 <riyad.rahman73@myhunter.cuny.edu>
2026-05-11 19:18:55 -0400

    feat: allow X-Guest-Id header

--- server/src/index.js
+++ server/src/index.js
@@ -67,7 +67,7 @@ app.get('/api/profile', async (req, res) => {
   } else {
     const sessionId = req.headers['X-Guest-Id'];
     if (sessionId === undefined || Array.isArray(sessionId)) {
-      res.json({ result: "not logged in" });
+      res.json({ result: "not logged in", guestId: sessionId });
     } else {
       const guest = await generateGuestWithId(sessionId);
       await data.addUser(guest);
